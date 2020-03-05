f50b5685975b791f340105b559ecc062a222b486
jessicaelee <jessica.e.lee.93@gmail.com>
2020-03-05 14:10:31 -0800

    ready to deploy frontend

--- frontend/src/JoblyAPI.js
+++ frontend/src/JoblyAPI.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 class JoblyApi {
   static async request(endpoint, paramsOrData = {}, verb = "get") {
@@ -8,7 +9,7 @@ class JoblyApi {
     try {
       return (await axios({
         method: verb,
-        url: `http://localhost:3001/${endpoint}`,
+        url: `${BASE_URL}/${endpoint}`,
         [verb === "get" ? "params" : "data"]: paramsOrData
       })).data;
       // axios sends query string data via the "params" key,
